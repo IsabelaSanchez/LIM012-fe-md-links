c1457d44932debb5d162eb304234fbcc31f785af
IsabelaSanchez <natyst_26@hotmail.com>
2020-05-11 14:04:02 -0500

    Fifth Commit: isDirectory function and test
    * New test about isDirectory function
    This function help us to identify if our path is a directory
    We used fs method
    Also we used fs.lstatSync to call the path
    isDirectory is a boolean which tell us if our path is a directory
    We add pruebaDeDIrectorio file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-const path = require('path');
+const path = require('path');//
+const fs = require('fs');//
 
 const beAbsolutePath = (route) => path.isAbsolute(route);
+// función para saber si ruta es absoluta
 
 const transIntoAbsolute = (route) => path.resolve(route);
-// eslint-disable-next-line no-console
-console.log(transIntoAbsolute('../pruebas/PRUEBA1.md'));
-// eslint-disable-next-line no-console
-console.log(beAbsolutePath('..\\PRUEBA2.md'));
-// eslint-disable-next-line no-console
-console.log(transIntoAbsolute('..\\PRUEBA2.md'));
-// eslint-disable-next-line no-console
-console.log(beAbsolutePath(transIntoAbsolute('..\\PRUEBA2.md')));
-module.exports = { beAbsolutePath, transIntoAbsolute };
+// función para transformar ruta relativa a absoluta
+
+const isDirectory = (route) => fs.lstatSync(route).isDirectory();
+// console.log(beDirectory('../pruebas/pruebaDeDIrectorio'));
+
+
+// console.log(transIntoAbsolute('../pruebas/PRUEBA1.md'));
+// console.log(beAbsolutePath('..\\PRUEBA2.md'));
+// console.log(transIntoAbsolute('..\\PRUEBA2.md'));
+// console.log(beAbsolutePath(transIntoAbsolute('..\\PRUEBA2.md')));
+
+module.exports = { beAbsolutePath, transIntoAbsolute, isDirectory };
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { beAbsolutePath, transIntoAbsolute } = require('../src/index.js');
+const { beAbsolutePath, transIntoAbsolute, isDirectory } = require('../src/index.js');
 
 const cwd = process.cwd();
 
@@ -12,7 +12,7 @@ describe('Testing para saber si beAbsolutePath es función', () => {
 describe('Testing para saber si la ruta es absoluta', () => {
   it('debería dar true si es ruta absoluta', () => {
     // eslint-disable-next-line no-console
-    console.log(beAbsolutePath(path.join(cwd, '\\src')));
+    // console.log(beAbsolutePath(path.join(cwd, '\\src')));
     expect(beAbsolutePath(path.join(cwd, '\\src'))).toBe(true);
   });
 
@@ -27,6 +27,18 @@ describe('Testing para convertir ruta relativa a ruta absoluta', () => {
   });
   it('debería convertir a ruta absoluta', () => {
     // console.log(transIntoAbsolute('..\PRUEBA2.md'));
-    expect(transIntoAbsolute('..\\PRUEBA2.md')).toBe('C:\\Users\\Isabella\\Documents\\Laboratoria-p\\LIM012-fe-md-links\\..PRUEBA2.md');
+    expect(transIntoAbsolute('../pruebas/PRUEBA1.md')).toBe('C:\\Users\\Isabella\\Documents\\Laboratoria-p\\pruebas\\PRUEBA1.md');
+  });
+});
+
+describe('Testing para saber si la ruta es directorio o archivo', () => {
+  it('isDirectory debería ser una función', () => {
+    expect(typeof isDirectory).toBe('function');
+  });
+  it('isDirectory debería dar true si es directorio', () => {
+    expect(isDirectory('C:\\Users\\Isabella\\Documents\\Laboratoria-p\\LIM012-FE-MD-LINKS\\pruebas')).toBe(true);
+  });
+  it('isDirectory debería dar false si es archivo', () => {
+    expect(isDirectory('C:\\Users\\Isabella\\Documents\\Laboratoria-p\\LIM012-FE-MD-LINKS\\pruebas\\PRUEBA1.md')).toBe(false);
   });
 });
